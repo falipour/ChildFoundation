744cb064503ac5c47ab24727be6d454587c5fe3d
Fatemeh <fatemehalpr@gmail.com>
2018-01-30 18:04:06 +0330

    karbar model

--- karbar/models.py
+++ karbar/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class MyUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.IntegerField(choices={(0, 'modir'), (1, 'madadkar'), (2, 'hamyar'), (3, 'madadju')})
+    national_id = models.IntegerField(max_length=10, unique=True, blank=True)
+
+    # address
+    country = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    postal_code = models.IntegerField(max_length=10)
+
+    phone_number = models.IntegerField(max_length=11)
+
+    def __str__(self):
+        return self.user.first_name + " " + self.user.last_name
