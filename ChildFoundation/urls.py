e8c2a8716535289628679bdadd31165a35aa6bf8
Fatemeh <fatemehalpr@gmail.com>
2018-01-30 17:50:48 +0330

    urls added

--- ChildFoundation/urls.py
+++ ChildFoundation/urls.py
@@ -1,7 +1,12 @@
-
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('karbar.urls')),
+    path('hamyar/', include('hamyar.urls')),
+    path('madadju/', include('madadju.urls')),
+    path('madadkar/', include('madadkar.urls')),
+    path('modir/', include('modir.urls')),
 ]
