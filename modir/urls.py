893e610d4cc6385f5ac401d78ad7118bd2b7c906
Fatemeh <fatemehalpr@gmail.com>
2018-01-30 21:51:54 +0330

    modir navbar

--- modir/urls.py
+++ modir/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('goals/', AdminGoalsView.as_view()),
+    path('', AdminHomeView.as_view(), name='admin-home'),
+    path('goals/', AdminGoalsView.as_view(), name='admin-goals'),
+    path('history/', AdminHistoryView.as_view(), name='admin-history'),
+    path('chart/', AdminChartView.as_view(), name='admin-chart'),
+    path('contact/', AdminContactView.as_view(), name='admin-contact'),
 ]
-
-
--- modir/views.py
+++ modir/views.py
@@ -3,3 +3,19 @@ from django.views.generic import TemplateView
 
 class AdminGoalsView(TemplateView):
     template_name = 'modir/Admin_Goals.html'
+
+
+class AdminHomeView(TemplateView):
+    template_name = 'modir/Admin_Home.html'
+
+
+class AdminContactView(TemplateView):
+    template_name = 'modir/Admin_Contact.html'
+
+
+class AdminHistoryView(TemplateView):
+    template_name = 'modir/Admin_History.html'
+
+
+class AdminChartView(TemplateView):
+    template_name = 'modir/Admin_Chart.html'
