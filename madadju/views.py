9b32f9579ba61b3a4fa47682324f056964cc23bf
niloofar <niloofar.zrf@gmail.com>
2018-01-31 18:04:33 +0330

    madadju templates foldered

--- madadju/views.py
+++ madadju/views.py
@@ -1,28 +1,28 @@
 from django.shortcuts import render
 
 def madadjuhome(request):
-    return render(request, 'madadju.html')
+    return render(request, 'madadju/madadju.html')
 
 def madadjugoal(request):
-    return render(request, 'madadju-goals.html')
+    return render(request, 'madadju/madadju-goals.html')
 
 def madadjuhistory(request):
-    return render(request, "madadju-history.html")
+    return render(request, "madadju/madadju-history.html")
 
 def madadjuchart(request):
-    return render(request, "madadju-chart.html")
+    return render(request, "madadju/madadju-chart.html")
 
 def madadjucontact(request):
-    return render(request, "madadju-contact.html")
+    return render(request, "madadju/madadju-contact.html")
 
 def madadkarchange(request):
-    return render(request, "madadkarchange.html")
+    return render(request, "madadju/madadkarchange.html")
 
 def madadjuprofile(request):
-    return render(request, "profile.html")
+    return render(request, "madadju/profile.html")
 
 def madadjumsg(request):
-    return render(request, "sendmsg.html")
+    return render(request, "madadju/sendmsg.html")
 
 def madadjureq(request):
-    return render(request, "sendreq.html")
+    return render(request, "madadju/sendreq.html")
