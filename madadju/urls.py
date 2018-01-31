273d22ebd953812d1634ef168ffe0c5568785ce5
niloofar <niloofar.zrf@gmail.com>
2018-01-31 23:20:08 +0330

    contact added

--- madadju/urls.py
+++ madadju/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url, include
 from django.urls import path
 from .views import *
-import madadju
+import madadju.views
 
 urlpatterns = [
     path('', madadju.views.madadjuhome, name='madadju-home'),
     path('goals/', madadju.views.madadjugoal, name='madadju-goals'),
     path('history/', madadju.views.madadjuhistory, name='madadju-history'),
     path('chart/', madadju.views.madadjuchart, name='madadju-chart'),
-    path('contact/', madadju.views.madadjucontact, name='madadju-contact'),
+    path('contact/', MadadjuContact.as_view(), name='madadju-contact'),
     path('madadkar-change/', madadju.views.madadkarchange, name='madadju-madadkar-change'),
     path('profile/',madadju.views.madadjuprofile,name='madadju-profile'),
     path('sendmsg/',madadju.views.madadjumsg,name='madadju-msg'),
